a32c3a4937ca97f12a5488352ae0c3d324e9a28f
Muhammad <mtanveer0118@outlook.com>
2021-01-10 12:31:29 -0500

    1.0.1 fixed deployment issues

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const passport = require("passport");
 const express = require("express");
 const app = express();
+const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
@@ -44,5 +45,13 @@ app.use("/api/bills", billsRouter);
 app.use("/api/expenses", expensesRouter);
 app.use("/api/income", incomeRouter);
 
+// (Heroku deployment)
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 //Port deployment
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(process.env.PORT || port, () => console.log(`Server running on port ${port}.`));
